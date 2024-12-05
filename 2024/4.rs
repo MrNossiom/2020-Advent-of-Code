ba36934498b1bf9b33572fbe65013faec217269a
Milo Moisson <milomoisson@gmail.com>
2024-12-05 18:12:47 +0100

    this was the fking bug

--- 2024/4.rs
+++ 2024/4.rs
@@ -1,6 +1,5 @@
 const COLS: usize = 140;
 
-// the needle must not overlap with it self
 fn count_needle(haystack: Vec<u8>, needle: &[u8]) -> usize {
 	let reverse_needle = needle.iter().rev().collect::<Vec<_>>();
 
@@ -9,7 +8,7 @@ fn count_needle(haystack: Vec<u8>, needle: &[u8]) -> usize {
 	let mut lines_flow_count = [0usize; COLS];
 	let mut lines_rev_count = [0usize; COLS];
 
-	for line in haystack.chunks(COLS) {
+	for line in haystack.chunks(COLS + 1) {
 		let mut flow_count = 0usize;
 		let mut rev_count = 0usize;
 
@@ -17,21 +16,13 @@ fn count_needle(haystack: Vec<u8>, needle: &[u8]) -> usize {
 
 		while index < line.len() {
 			let chr = line[index];
-			let mut should = false;
 			// .zip(lines_flow_count.as_mut())
 			// .zip(lines_rev_count.as_mut())
-			if needle[flow_count..flow_count + 4] == *b"XMAS" {
-				should = true;
-			}
 			if chr == needle[flow_count] {
 				if flow_count == needle.len() - 1 {
 					flow_count = 0;
 					total += 1;
 				} else {
-					if should {
-						dbg!(&needle[flow_count..flow_count + 4]);
-					}
-
 					flow_count += 1;
 				}
 			} else if flow_count != 0 && chr == needle[0] {
