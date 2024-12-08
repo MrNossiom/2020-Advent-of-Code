fcb071899c1590a726ab69bb5896abbf2dc0ef78
Milo Moisson <milomoisson@gmail.com>
2024-12-08 01:05:45 +0100

    feat: finish day5 of 2024

--- 2024/05.rs
+++ 2024/05.rs
@@ -1,30 +1,70 @@
+use std::cmp::Ordering;
 use std::collections::HashMap;
+
+fn is_ordered(index: usize, elem: u16, update: &[u16], map: &HashMap<u16, Vec<u16>>) -> bool {
+	let Some(matching_pages) = map.get(&elem) else {
+		return true;
+	};
+
+	for matching_page in matching_pages {
+		let mut j = 0;
+		while j < index {
+			if update[j] == *matching_page {
+				return false;
+			}
+			j += 1;
+		}
+	}
+
+	return true;
+}
 
 fn check_ordering_in_updates(updates: &Vec<Vec<u16>>, ordering: &HashMap<u16, Vec<u16>>) -> u64 {
 	let mut count = 0;
 
 	for update in updates {
-		let is_correctly_ordered = update.iter().enumerate().all(|(i, elem)| {
-			let Some(matching_pages) = ordering.get(elem) else {
-				return true;
-			};
-
-			for matching_page in matching_pages {
-				let mut j = 0;
-				while j < i {
-					if update[j] == *matching_page {
-						return false;
-					}
-					j += 1;
-				}
-			}
-
-			return true;
-		});
+		let is_correctly_ordered = update
+			.iter()
+			.enumerate()
+			.all(|(i, elem)| is_ordered(i, *elem, update, ordering));
 
 		if is_correctly_ordered {
 			count += u64::from(update[update.len() / 2]);
 		}
+	}
+
+	count
+}
+
+fn bar(updates: &Vec<Vec<u16>>, ordering: &HashMap<u16, Vec<u16>>) -> u64 {
+	let mut count = 0;
+
+	for update in updates {
+		let is_correctly_ordered = update
+			.iter()
+			.enumerate()
+			.all(|(i, elem)| is_ordered(i, *elem, update, ordering));
+
+		if is_correctly_ordered {
+			continue;
+		}
+
+		let mut update = update.clone();
+
+		update.sort_by(|a, b| {
+			ordering
+				.get(a)
+				.map(|arr| {
+					if arr.iter().find(|el| *el == b).is_some() {
+						Ordering::Less
+					} else {
+						Ordering::Greater
+					}
+				})
+				.unwrap_or(Ordering::Greater)
+		});
+
+		count += u64::from(update[update.len() / 2]);
 	}
 
 	count
@@ -53,4 +93,5 @@ fn main() {
 		.collect::<Vec<Vec<u16>>>();
 
 	let _silver = dbg!(check_ordering_in_updates(&updates, &ordering_rules));
+	let _golden = dbg!(bar(&updates, &ordering_rules));
 }
